af6645839e46eb8686c30c42d2b26e9eda939399
anthony sanchez <anthonysa0813@gmail.com>
2021-11-28 15:49:41 -0500

    add bootstrap

---
+++ bootstrap/main.js
@@ -0,0 +1,24 @@
+
+// url = endpoints = api
+const api = "https://misproductos/api/"
+const products = [
+  {
+    name: "laptop lenovo",
+    price: 1500,
+  },
+  {
+    name: "laptop HP",
+    price: 1550,
+  },
+  {
+    name: "laptop ASUS",
+    price: 1900,
+  },
+  {
+    img: "api/ruta.png",
+    name: "laptop MAC",
+    price: 2500,
+  },
+];
+
+
